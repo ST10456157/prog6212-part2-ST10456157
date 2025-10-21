36b045e21f5cf6504d7d019efa4fe12933edd7ab
Keshav Haripaul <keshav.haripaul@yahoo.com>
2025-10-21 16:13:36 +0200

    Add project files.

---
+++ prog6212-part2-ST10456157/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prog6212_part1_ST10456157.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ prog6212-part2-ST10456157/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prog6212_part2_ST10456157.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
