de3acad8100fa273da6a545d9602b13cffe751bc
Keshav Haripaul <keshav.haripaul@yahoo.com>
2025-10-21 16:47:13 +0200

    SET UP ENTITY FRAMEWORK CORE

---
+++ prog6212-part2-ST10456157/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+namespace prog6212_part2_ST10456157.Models
+{
+
+    namespace prog6212_part2_ST10456157.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Claim> Claims { get; set; }
+    }
+}
