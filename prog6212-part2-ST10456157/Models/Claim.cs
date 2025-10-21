1ad5d77ecf196323abfdbfd4f228367526340215
Keshav Haripaul <keshav.haripaul@yahoo.com>
2025-10-21 17:18:51 +0200

    Added claim model

---
+++ prog6212-part2-ST10456157/Models/Claim.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prog6212_part2_ST10456157.Models
+{
+    public class Claim
+    {
+        [Key]
+        public int ClaimId { get; set; }
+
+        [Required]
+        public string LecturerName { get; set; }
+
+        [Required]
+        public decimal HoursWorked { get; set; }
+
+        [Required]
+        public decimal HourlyRate { get; set; }
+
+        public string? Notes { get; set; }
+        public string? DocumentName { get; set; }
+        public string Status { get; set; } = "Pending";
+        public DateTime DateSubmitted { get; set; } = DateTime.Now;
+    }
+}
